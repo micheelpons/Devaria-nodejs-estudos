90523b4fa705964ad113baca0b79270fce374bdf
Michel Pons <111322806+micheelpons@users.noreply.github.com>
2022-09-06 08:19:17 -0300

    exercício-de-estrutura-de-repetição

---
+++ estrutura-de-repeticao/app.js
@@ -0,0 +1,24 @@
+const listaArgumentos = process.argv.slice(2);
+
+console.log('------------Executando um FOR-----------------');
+for(let controladorFor = 0; controladorFor < listaArgumentos.length; controladorFor++){
+    console.log(`Posição ${controladorFor} valor lido = ${listaArgumentos[controladorFor]}`)
+}
+
+console.log('------------Executando um WHILE-----------------');
+let controladorWhile = 0;
+while(controladorWhile < listaArgumentos.length){
+    console.log(`Posição ${controladorWhile} valor lido = ${listaArgumentos[controladorWhile]}`);
+    controladorWhile++;
+}
+console.log('------------Executando um DO WHILE-----------------');
+let controladorDoWhile = 0;
+do{
+    console.log(`Posição ${controladorDoWhile} valor lido = ${listaArgumentos[controladorDoWhile]}`);
+    controladorDoWhile++;
+}while(controladorDoWhile < listaArgumentos.length)
+
+console.log('------------Executando um FOF OF-----------------');
+for(const argumento of listaArgumentos){
+    console.log(`Valor lido = ${argumento}`);
+}
